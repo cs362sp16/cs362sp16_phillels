c274c5d1e020c5b531689df2b7fefd2a409d734b
Elsie Phillips <Mae@10-251-51-212.wireless.oregonstate.edu>
2016-06-02 21:58:43 -0700

    change myassert to track with global variable optionally

--- dominion/myassert.h
+++ dominion/myassert.h
@@ -2,12 +2,25 @@
 #include "stdio.h"
 #include "dominion.h"
 
-#define newassert(condition) \
-{ \
-	if(!(condition)) \
+#ifdef ASSERT_TRACK
+	#define newassert(condition) \
 	{ \
-		fprintf(stderr, "Assertion failed at %s inside %s: \n\tCondition: %s\n",\
-		__FILE__, __FUNCTION__, (#condition)); \
-	} \
-}
+		if(!(condition)) \
+		{ \
+			fprintf(stderr, "Incrementing assert.\nGiraffe failed at %s inside %s: \n\tCondition: %s\n",\
+			__FILE__, __FUNCTION__, (#condition)); \
+			asserttrack++; \
+		} \
+	}
+#else
+	#define newassert(condition) \
+	{ \
+		if(!(condition)) \
+		{ \
+			fprintf(stderr, "Giraffe failed at %s inside %s: \n\tCondition: %s\n",\
+			__FILE__, __FUNCTION__, (#condition)); \
+		} \
+	}
+#endif
+
 #endif
